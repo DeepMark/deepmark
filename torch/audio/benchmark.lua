282be01df0fcec0cb2e3166ec52c72627a27b524
SeanNaren <seannaren@hotmail.com>
2016-08-04 10:54:09 +0100

    Buffer to store cuda inputs

--- torch/audio/benchmark.lua
+++ torch/audio/benchmark.lua
@@ -24,33 +24,36 @@ local dataset = nn.DeepSpeechDataset(batchSize)
 
 collectgarbage()
 local model, model_name, calculateInputSizes = deepSpeech(batchSize, dataset.freqBins, nGPU)
+local inputs = torch.CudaTensor() -- buffer for inputs
 local sizes, input, targets = dataset:nextTorchSet()
 
 model = model:cuda()
-input = input:cuda()
+inputs:resize(input:size()):copy(input)
+
 print('ModelType: ' .. model_name, 'Kernels: ' .. 'cuDNN')
 
 -- dry-run
 for i = 1, nDryRuns do
     model:zeroGradParameters()
-    local output = model:updateOutput(input)
-    local gradInput = model:updateGradInput(input, output)
-    model:accGradParameters(input, output)
+    local output = model:updateOutput(inputs)
+    local gradInput = model:updateGradInput(inputs, output)
+    model:accGradParameters(inputs, output)
     cutorch.synchronize()
     collectgarbage()
 end
 
-local tmf, tmbi, tmbg = 0, 0, 0
+local tmf, tmbi, tmbg, tRoundTrip = 0, 0, 0, 0
 local ok = 1
 for t = 1, steps do
+    local roundTripTimer = torch.Timer()
     dataset = nn.DeepSpeechDataset(batchSize)
     local numberOfIterations = 0
     local sizes, input, targets = dataset:nextTorchSet()
     while (sizes ~= nil) do
-        input = input:cuda()
+        inputs:resize(input:size()):copy(input)
         sys.tic()
         -- Forward through model and then criterion.
-        local output = model:updateOutput(input)
+        local output = model:updateOutput(inputs)
         loss = criterion:updateOutput(output, targets, calculateInputSizes(sizes))
 
         tmf = tmf + sys.toc()
@@ -59,13 +62,13 @@ for t = 1, steps do
         -- Backwards (updateGradInput, accGradParameters) including the criterion.
         sys.tic()
         grads = criterion:updateGradInput(output, targets)
-        model:updateGradInput(input, output)
+        model:updateGradInput(inputs, output)
         tmbi = tmbi + sys.toc()
         cutorch.synchronize()
 
         collectgarbage()
         sys.tic()
-        ok = pcall(function() model:accGradParameters(input, output) end)
+        ok = pcall(function() model:accGradParameters(inputs, output) end)
         tmbg = tmbg + sys.toc()
         cutorch.synchronize()
         sizes, input, targets = dataset:nextTorchSet()
@@ -76,10 +79,13 @@ for t = 1, steps do
     tmf = tmf / numberOfIterations
     tmbi = tmbi / numberOfIterations
     tmbg = tmbi / numberOfIterations
+    -- Add time taken for round trip of 1 epoch
+    tRoundTrip = tRoundTrip + roundTripTimer:time().real
 end
 tmf = tmf / steps
 tmbi = tmbi / steps
 tmbg = tmbi / steps
+tRoundTrip = tRoundTrip / steps
 print(string.format("%-30s %25s %10.2f", 'cuDNN', ':updateOutput():', tmf * 1000))
 print(string.format("%-30s %25s %10.2f", 'cuDNN', ':updateGradInput():', tmbi * 1000))
 
@@ -91,6 +97,7 @@ end
 print(string.format("%-30s %25s %10.2f", 'cuDNN', ':Forward:', (tmf) * 1000))
 print(string.format("%-30s %25s %10.2f", 'cuDNN', ':Backward:', (tmbi + tmbg) * 1000))
 print(string.format("%-30s %25s %10.2f", 'cuDNN', ':TOTAL:', (tmf + tmbi + tmbg) * 1000))
+print(string.format("%-30s %25s %10.2f", 'cuDNN', ':ROUNDTRIP TOTAL:', tRoundTrip * 1000))
 print()
 
 print('')
