3f1507ef27244644cbac370e8bfc08b15fcb4f16
Natalia Gimelshein <ngimelshein@gmail.com>
2016-11-01 17:03:01 -0700

    fix multi-GPU benchmark

--- torch/image+video/benchmark.lua
+++ torch/image+video/benchmark.lua
@@ -18,14 +18,28 @@ local opt = pl.lapp[[
 local net, isize = require(opt.network)(opt.batchSize)
 
 -- randomly initialized input
-local input  = torch.randn(unpack(isize))
+local sizeTensor = torch.LongTensor(isize)
+local optsizeTensor = sizeTensor:clone()
+optsizeTensor[1] = 4
+local input = torch.randn(sizeTensor:storage())
+local dummyinput = torch.randn(optsizeTensor:storage())
 
 -- random target classes from  1 to 100
 local target = torch.Tensor(input:size(1)):random(1, 100)
 
 -- cast network
 net:cuda()
-input, target = input:cuda(), target:cuda()
+if opt.nGPU == 1 then
+  input = input:cuda()
+else
+  local tmp = cutorch.createCudaHostTensor()
+  tmp:resize(input:size()):copy(input)
+  input = tmp
+end 
+if target.cudaLong then target = target:cudaLong() else target = target:cuda() end
+dummyinput = dummyinput:cuda()
+
+   
 
 if opt.backend == 'cudnn' then
    require 'cudnn'
@@ -35,16 +49,18 @@ if opt.backend == 'cudnn' then
 end
 
 -- optimize memory
-optnet.optimizeMemory(net, input, {inplace=true, mode='training', reuseBuffers=true})
+optnet.optimizeMemory(net, dummyinput, {inplace=true, mode='training', reuseBuffers=true})
 
 if opt.nGPU > 1 then
    assert(opt.nGPU <= cutorch.getDeviceCount(), 'number of GPUs less than nGPU specified')
    if opt.network == 'alexnet' or opt.network == 'vgg_d' or opt.network == 'c3d' then
       local features_single = net:get(1)
       local features = nn.DataParallelTable(1, true, true)
+      local fastest, benchmark = cudnn.fastest, cudnn.benchmark
       features:threads(function()
    	    require 'cunn'
    	    require 'cudnn'
+            cudnn.fastest, cudnn.benchmark = fastest, benchmark
       end)
       local gpuid = torch.range(1, opt.nGPU):totable()
       features:add(features_single, gpuid)
@@ -52,16 +68,19 @@ if opt.nGPU > 1 then
       net.modules[1] = features
    else
       net_single = net
-      local net = nn.DataParallelTable(1, true, true)
+      net = nn.DataParallelTable(1, true, true)
+      local fastest, benchmark = cudnn.fastest, cudnn.benchmark
       net:threads(function()
    	    require 'cunn'
    	    require 'cudnn'
+            cudnn.fastest, cudnn.benchmark = fastest, benchmark
       end)
       local gpuid = torch.range(1, opt.nGPU):totable()
       net:add(net_single, gpuid)
       net.gradInput = nil
    end
 end
+
 
 local params, gradParams = net:getParameters()
 
@@ -86,17 +105,17 @@ local tm = torch.Timer()
 -- dry-run
 for i = 1, opt.dryrun do
    optim.sgd(feval, params, optimState)
-   cutorch.synchronize()
+   cutorch.synchronizeAll()
    collectgarbage()
 end
 
 tm:reset()
 for t = 1, opt.iterations do
    optim.sgd(feval, params, optimState)
-   cutorch.synchronize()
+   cutorch.synchronizeAll()
 end
 local time_taken_per_iter = tm:time().real / opt.iterations
-local examples_per_sec = 1 / time_taken_per_iter * opt.batchSize * opt.nGPU
+local examples_per_sec = 1 / time_taken_per_iter * opt.batchSize 
 
 print(string.format("Device: %1d x %-15s    Network: %-15s      Backend: %-10s      " ..
                     "Batchsize/GPU: %-5d      Iter (ms): %-10.2f Examples/sec: %-5d",
